b190f22080958c08f27bc5ed3285e234e0e768c5
ryouichi-kurihara <ryouichi.kurihara.628@gmail.com>
2022-03-13 15:17:45 +0900

    練習

--- python/main.py
+++ python/main.py
@@ -1,5 +1,18 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from PIL import Image
+import os
 
 st.title('streamlit 超入門')
+
+st.write('DataFrame')
+
+# df = pd.DataFrame(np.random.rand(100,2)/[50, 50] + [35.69, 139.70], columns=['lat','lon'])
+
+# st.dataframe(df.style.highlight_min(), width=200, height=200)
+# st.map(df)
+st.write(os.getcwd())
+
+img = Image.open('aragaki.jpg')
+st.image(img, caption='kuri', use_column_width=True)
